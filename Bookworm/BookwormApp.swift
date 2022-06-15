0117d7f1e07cec8b3940ee91410a63c26819c5fc
Carson <carsongro@gmail.com>
2022-06-14 19:08:46 -0600

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Carson Gross on 6/14/22.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
